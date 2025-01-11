34f9d891414f78585d697b501940a9f5ae1d0e42
Joshua Esguerra <100118428+jvesguerra@users.noreply.github.com>
2025-01-11 12:20:21 +0800

    feat: csv to json

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,7 +1,8 @@
 import tweepy
 import requests
-
+import csv
 import os
+import json
 #from dotenv import load_dotenv
 
 # Load the .env file
@@ -46,14 +47,48 @@ params = {
     "max_results": max_results  # Max is 100 for free-tier
 }
 
-# Make the request
-response = requests.get(endpoint, headers=headers, params=params)
+# Path to the input CSV file
+csv_file_path = 'testData1.csv'  # Replace with the actual file path
 
-if response.status_code == 200:
-    tweets = response.json()
-    for tweet in tweets['data']:
-        print(f"Tweet: {tweet['text']}")
-else:
-    print(f"Error: {response.status_code}")
+# Read CSV data
+csv_data = []
+with open(csv_file_path, mode='r', encoding='utf-8') as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        csv_data.append(row)
+        
+# Make the request
+#response = requests.get(endpoint, headers=headers, params=params)
+
+# if response.status_code == 200:
+#     tweets = response.json()
+#     for tweet in tweets['data']:
+#         print(f"Tweet: {tweet['text']}")
+# else:
+#     print(f"Error: {response.status_code}")
+
+
+# Path to save JSON file
+json_file_path = 'tweets.json'
+
+# Open a JSON file to write the data
+with open(json_file_path, 'w', encoding='utf-8') as file:
+    # Save the data directly in JSON format
+    json.dump(csv_data, file, indent=4)
+
+print(f"Tweets have been saved to {json_file_path}")
+    
+# Scraped data to JSON
+# if response.status_code == 200:
+#     tweets = response.json()
+
+#     # Open a JSON file to write the data
+#     with open(json_file_path, 'w', encoding='utf-8') as file:
+#         # Save the data directly in JSON format
+#         json.dump(tweets, file, indent=4)
+
+#     print(f"Tweets have been saved to {json_file_path}")
+# else:
+#     print(f"Error: {response.status_code}")
     
 
